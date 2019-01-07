000acf3949e3fff99539266a07dffbdae15af2e5
wangdai <37567241+Davizss@users.noreply.github.com>
2019-01-07 09:04:21 +0800

    use requests

--- youdao.py
+++ youdao.py
@@ -1,16 +1,17 @@
-from urllib import request, parse
 import time
 import random
 import hashlib
 import json
+import requests
+
 
 def md5(value):
     m = hashlib.md5()
     m.update(bytes(value,encoding='utf-8'))
     return m.hexdigest()
 
-base_url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
 
+base_url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
 keyword = input('输入翻译：')
 salt = int(time.time() * 1000) + random.randint(0,9)
 sign = "fanyideskweb" + keyword + str(salt) + "aNPG!!u6sesA>hBAW1@(-"
@@ -31,12 +32,11 @@ data = {
     "typoResult": "false",
     "version": "2.1",
 }
-data = parse.urlencode(data)
 
 headers = {
     "Host": "fanyi.youdao.com",
     "Connection": "keep-alive",
-    "Content-Length": len(data),
+    "Content-Length": str(len(data)),
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Origin": "http://fanyi.youdao.com",
     "X-Requested-With": "XMLHttpRequest",
@@ -48,9 +48,9 @@ headers = {
     "Cookie": "OUTFOX_SEARCH_USER_ID=1013580002@10.169.0.84; OUTFOX_SEARCH_USER_ID_NCOO=216555653.08909458; fanyi-ad-id=39535; fanyi-ad-closed=1; JSESSIONID=aaaqzHsRj0pfddFyj8edw; UM_distinctid=160c4f0adef526-00416b6000ee1a-b7a103e-fa000-160c4f0adf0501; ___rl__test__cookies=1515132715362",
 }
 
-req = request.Request(url=base_url,data=bytes(data,'utf-8'),headers=headers)
-response = request.urlopen(req)
-data = response.read().decode('utf-8')
+
+response = requests.post(base_url, data=data, headers=headers)
+data = response.text
 data = json.loads(data)
 res = data['translateResult'][0][0]['tgt']
 
